7bdcf9b6cda19c522697f97cf3930acfd420d610
ynekete <69242105+ynekete@users.noreply.github.com>
2020-09-01 15:04:38 +0300

    Add files via upload

--- Lab1.java
+++ Lab1.java
@@ -5,6 +5,7 @@
  */
 package lab1;
 import java.util.Scanner;
+import java.lang.Math;
 /**
  *
  * @author student
@@ -49,6 +50,39 @@ public class Lab1 {
 //                System.out.println(a);
 //                a=a+1/(n);           
 //            }
-//            
+//  
+//Сгенерировать массив целых чисел случайным образом, вывести его на экран, отсортировать его, и снова вывести на экран
+
+//                int n,arr[];
+//                Scanner in = new Scanner(System.in);
+//                System.out.println("Введите размер массива");
+//                n = in.nextInt();
+//                arr = new int [n];
+//                for ( int i = 0; i < n; i++){
+//                    arr[i] = ((int)(Math.random()*100));
+//                    System.out.println(arr[i] );
+//                }
+//                
+//                System.out.println();
+//                System.out.println();
+//                
+//                for (int i = 0; i < n-1; i++) 
+//                    for (int j = 0; j < n-i-1; j++) 
+//                        if (arr[j] > arr[j+1]) { 
+//                            int temp = arr[j]; 
+//                            arr[j] = arr[j+1]; 
+//                            arr[j+1] = temp; 
+//                        }
+//                for ( int s = 0; s < arr.length; s++){                   
+//                    System.out.println(arr[s] );
+//                }    
+//Факториал числа циклом
+//                  Scanner scanner = new Scanner(System.in);
+//                  int num = scanner.nextInt(); 
+//                  int i = 1;
+//                  for (int o = 1; o <=num; o ++){
+//			i*=o;
+//		}                  
+//                  System.out.println(i);
+        }
     }
-}
